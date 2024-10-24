086731af27fdd04cdcc7d48290012e5748b5df94
jm-willy <jaimewilly@hotmail.es>
2024-10-24 13:03:33 +0200

    tabla de recibos

--- flutter_application_1/lib/recibos.dart
+++ flutter_application_1/lib/recibos.dart
@@ -122,6 +122,54 @@ class ReceiptPage extends StatelessWidget {
       'acidez': '0.50',
       'calidad': 'extra',
     },
+    {
+      'albaran': '30',
+      'socio': '2456',
+      'date': '21/10/2024',
+      'poligono': '8',
+      'parcela': '32',
+      'neto': '890',
+      'rendto': '10',
+      'humedad': '1.00',
+      'acidez': '0.50',
+      'calidad': 'extra',
+    },
+    {
+      'albaran': '30',
+      'socio': '2456',
+      'date': '21/10/2024',
+      'poligono': '8',
+      'parcela': '32',
+      'neto': '890',
+      'rendto': '10',
+      'humedad': '1.00',
+      'acidez': '0.50',
+      'calidad': 'extra',
+    },
+    {
+      'albaran': '30',
+      'socio': '2456',
+      'date': '21/10/2024',
+      'poligono': '8',
+      'parcela': '32',
+      'neto': '890',
+      'rendto': '10',
+      'humedad': '1.00',
+      'acidez': '0.50',
+      'calidad': 'extra',
+    },
+    {
+      'albaran': '30',
+      'socio': '2456',
+      'date': '21/10/2024',
+      'poligono': '8',
+      'parcela': '32',
+      'neto': '890',
+      'rendto': '10',
+      'humedad': '1.00',
+      'acidez': '0.50',
+      'calidad': 'extra',
+    },
   ];
 
   @override
@@ -131,40 +179,59 @@ class ReceiptPage extends StatelessWidget {
         title: const Text('Recibos y Albaranes'),
         backgroundColor: const Color.fromARGB(255, 190, 228, 164),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical, // Habilita scroll vertical
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal, // Habilita scroll horizontal
-            child: DataTable(
-              showCheckboxColumn: true,
-              columns: const [
-                DataColumn(label: Text('Albarán')),
-                DataColumn(label: Text('Socio')),
-                DataColumn(label: Text('Fecha')),
-                DataColumn(label: Text('Polígono')),
-                DataColumn(label: Text('Parcela')),
-                DataColumn(label: Text('Neto (kg)')),
-                DataColumn(label: Text('Rendimiento (%)')),
-                DataColumn(label: Text('Humedad (%)')),
-                DataColumn(label: Text('Acidez (%)')),
-                DataColumn(label: Text('Calidad')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(9.0),
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 249, 255, 245),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(110, 78, 81, 75),
+                  spreadRadius: 3,
+                  blurRadius: 6,
+                  offset: Offset(1, 2), // Cambia posicion de la sombra
+                ),
               ],
-              rows: receipts.map((receipt) {
-                return DataRow(cells: [
-                  DataCell(Text(receipt['albaran'])),
-                  DataCell(Text(receipt['socio'])),
-                  DataCell(Text(receipt['date'])),
-                  DataCell(Text(receipt['poligono'])),
-                  DataCell(Text(receipt['parcela'])),
-                  DataCell(Text(receipt['neto'])),
-                  DataCell(Text(receipt['rendto'])),
-                  DataCell(Text(receipt['humedad'])),
-                  DataCell(Text(receipt['acidez'])),
-                  DataCell(Text(receipt['calidad'])),
-                ]);
-              }).toList(),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical, // Habilita scroll vertical
+                child: SingleChildScrollView(
+                  scrollDirection:
+                      Axis.horizontal, // Habilita scroll horizontal
+                  child: DataTable(
+                    columns: const [
+                      DataColumn(label: Text('Albarán')),
+                      DataColumn(label: Text('Socio')),
+                      DataColumn(label: Text('Fecha')),
+                      DataColumn(label: Text('Polígono')),
+                      DataColumn(label: Text('Parcela')),
+                      DataColumn(label: Text('Neto (kg)')),
+                      DataColumn(label: Text('Rendimiento (%)')),
+                      DataColumn(label: Text('Humedad (%)')),
+                      DataColumn(label: Text('Acidez (%)')),
+                      DataColumn(label: Text('Calidad')),
+                    ],
+                    rows: receipts.map((receipt) {
+                      return DataRow(cells: [
+                        DataCell(Text(receipt['albaran'])),
+                        DataCell(Text(receipt['socio'])),
+                        DataCell(Text(receipt['date'])),
+                        DataCell(Text(receipt['poligono'])),
+                        DataCell(Text(receipt['parcela'])),
+                        DataCell(Text(receipt['neto'])),
+                        DataCell(Text(receipt['rendto'])),
+                        DataCell(Text(receipt['humedad'])),
+                        DataCell(Text(receipt['acidez'])),
+                        DataCell(Text(receipt['calidad'])),
+                      ]);
+                    }).toList(),
+                  ),
+                ),
+              ),
             ),
           ),
         ),
