d74670eb002e3f0a7c28017f31fdebbb55ba74e0
jm-willy <jaimewilly@hotmail.es>
2024-10-30 15:34:02 +0100

    scroll para temporadas

--- lib/home.dart
+++ lib/home.dart
@@ -9,89 +9,100 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<String> temporadas = [
+      'Temporada 2020',
       'Temporada 2021',
       'Temporada 2022',
       'Temporada 2023',
       'Temporada 2024',
+      'Temporada 2025',
+      'Temporada 2020',
+      'Temporada 2021',
+      'Temporada 2022',
+      'Temporada 2023',
+      'Temporada 2024',
+      'Temporada 2025',
     ];
 
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(6.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text(
-              'Seleccione una temporada para ver datos:',
-              style: TextStyle(fontSize: 23, fontWeight: FontWeight.w900),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            for (var temporada in temporadas)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    elevation: 4,
-                    padding: const EdgeInsets.symmetric(vertical: 22),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Seleccione una temporada para ver datos:',
+                style: TextStyle(fontSize: 23, fontWeight: FontWeight.w900),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              for (var temporada in temporadas)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 4,
+                      padding: const EdgeInsets.symmetric(vertical: 22),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      backgroundColor: myButtonColor,
                     ),
-                    backgroundColor: myButtonColor,
-                  ),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      barrierColor: Color.fromARGB(133, 0, 11, 0),
-                      builder: (BuildContext context) {
-                        return Dialog(
-                          backgroundColor: Colors.transparent,
-                          insetPadding: EdgeInsets.zero,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(20),
-                            child: SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.94,
-                              height: MediaQuery.of(context).size.height * 0.94,
-                              child: Scaffold(
-                                backgroundColor: myScaffoldColor,
-                                appBar: AppBar(
-                                  backgroundColor: myButtonColor,
-                                  title: Text('Datos de $temporada'),
-                                  actions: [
-                                    IconButton(
-                                      icon: const Icon(Icons.close),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        barrierColor: Color.fromARGB(133, 0, 11, 0),
+                        builder: (BuildContext context) {
+                          return Dialog(
+                            backgroundColor: Colors.transparent,
+                            insetPadding: EdgeInsets.zero,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.94,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.94,
+                                child: Scaffold(
+                                  backgroundColor: myScaffoldColor,
+                                  appBar: AppBar(
+                                    backgroundColor: myButtonColor,
+                                    title: Text('Datos de $temporada'),
+                                    actions: [
+                                      IconButton(
+                                        icon: const Icon(Icons.close),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                  body: ReceiptPage(),
                                 ),
-                                body: ReceiptPage(),
                               ),
                             ),
+                          );
+                        },
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.calendar_today, color: Colors.white),
+                        const SizedBox(width: 10),
+                        Text(
+                          temporada,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
                           ),
-                        );
-                      },
-                    );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(Icons.calendar_today, color: Colors.white),
-                      const SizedBox(width: 10),
-                      Text(
-                        temporada,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
